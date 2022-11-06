2cbb43043e50ec499b7282bb5e8c50d0d7d03b20
serkan <serkan.urganci10@gmail.com>
2022-11-07 00:56:51 +0300

    trailer pop up

--- src/components/Row.js
+++ src/components/Row.js
@@ -2,9 +2,13 @@ import React, { useEffect, useState } from "react";
 import axios from "@/axios";
 import "./Row.scss";
 import { image_base_url } from "@/helpers";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 function Row({ title, apiUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerVideoId, setTrailerVideoId] = useState("");
+
   useEffect(() => {
     const fetchData = async () => {
       const request = await axios.get(apiUrl);
@@ -13,12 +17,34 @@ function Row({ title, apiUrl, isLargeRow }) {
     fetchData();
   }, [apiUrl]);
 
+  const handleOnClickMoviePoster = (movie) => {
+    if (trailerVideoId) {
+      setTrailerVideoId("");
+    } else {
+      movieTrailer(movie?.title || movie?.name || movie?.original_name || "")
+        .then((url) => {
+          const params = new URLSearchParams(new URL(url).search);
+          setTrailerVideoId(params.get("v"));
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
   return (
     <div className="row">
       <h2>{title}</h2>
       <div className="row__posters">
         {movies.map((movie) => (
           <img
+            onClick={() => handleOnClickMoviePoster(movie)}
             onError={(e) => (e.target.style.display = "none")}
             key={movie.id}
             alt={movie.name}
@@ -29,6 +55,7 @@ function Row({ title, apiUrl, isLargeRow }) {
           />
         ))}
       </div>
+      {trailerVideoId && <YouTube videoId={trailerVideoId} opts={opts} />}
     </div>
   );
 }
