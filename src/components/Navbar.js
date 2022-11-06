10ff6e69f9029cd4912c50871f359bc14233e13b
serkan <serkan.urganci10@gmail.com>
2022-11-07 00:17:47 +0300

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import "./Navbar.scss";
+
+function Navbar() {
+  const [showNavBlack, setShowNavBlack] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      console.log("listener");
+      if (window.scrollY > 100) {
+        setShowNavBlack(true);
+      } else {
+        setShowNavBlack(false);
+      }
+      return () => {
+        window.removeEventListener("scroll");
+      };
+    });
+  }, []);
+  return (
+    <div className={`navbar ${showNavBlack && "navbar__black"}`}>
+      <img
+        className="navbar__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
+        alt="logo"
+      />
+      <img
+        className="navbar__avatar"
+        src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
+        alt="logo"
+      />
+    </div>
+  );
+}
+
+export default Navbar;
